dd1c961a50abd97798e302c73a5ba8b59940171e
Lerros <108396158+LerrosGr@users.noreply.github.com>
2024-06-19 14:08:19 +0300

    rework home

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,12 +1,32 @@
 import { Link, useLocation } from 'react-router-dom';
+import css from './MovieList.module.css';
 
 export default function MovieList({ movies }) {
   const location = useLocation();
   return (
-    <ol>
+    <ol className={css.mainList}>
       {movies.map(movie => (
-        <li key={movie.id}>
+        <li className={css.listItem} key={movie.id}>
+          <div className={css.rating}>4.5</div>
           <Link
+            to={`${location.pathname === '/movies' ? '' : 'movies/'}${
+              movie.id
+            }`}
+            state={location}
+          >
+            <img
+              className={css.poster}
+              src={
+                movie.poster_path
+                  ? `https://image.tmdb.org/t/p/w300${movie.poster_path}`
+                  : `http://www.suryalaya.org/images/no_image.jpg`
+              }
+              alt="poster image"
+            />
+          </Link>
+
+          <Link
+            className={css.title}
             to={`${location.pathname === '/movies' ? '' : 'movies/'}${
               movie.id
             }`}
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,6 +4,8 @@ import MovieList from '../../components/MovieList/MovieList';
 import ErrorMessage from '../../components/ErrorMessage/ErrorMessage';
 import { getMovies } from '../../movie-api';
 
+import css from './HomePage.module.css';
+
 export default function HomePage() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -15,6 +17,7 @@ export default function HomePage() {
         setIsLoading(true);
         setIsError(false);
         const data = await getMovies();
+        console.log(data);
         setMovies(data);
       } catch {
         setIsError(true);
@@ -25,7 +28,7 @@ export default function HomePage() {
     fetchMovies();
   }, []);
   return (
-    <div>
+    <div className={css.container}>
       {isError && <ErrorMessage />}
       {isLoading && <p>Loading...</p>}
       {movies.length > 0 && <MovieList movies={movies} />}
